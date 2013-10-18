018e41f99f7f2a7ba744698660c5ad6e4b325ccd
Joseph Lenton <josephlenton@studiofortress.com>
2013-10-18 11:42:04 +0100

    added .greek.js setup file to my-vim

---
+++ .greek.js
@@ -0,0 +1,10 @@
+"use strict";
+
+greek.
+        openProjectsBar().
+        setDefaultFolder( 'c:\\users\\joseph\\projects' ).
+        openWith( 'html', 'gvim', 'chrome' ).
+        openWith( 'hta', 'gvim', null ).
+        openWith( 'ts', null, 'gvim' ).
+        openWith( 'vbs', 'gvim', null );
+
