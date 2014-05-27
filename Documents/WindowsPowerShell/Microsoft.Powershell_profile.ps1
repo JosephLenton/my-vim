c101f621defcb9672719f744a973639199102d0c
Joseph Lenton <josephlenton@studiofortress.com>
2014-05-27 06:00:54 +0100

    added 'resize' function for resizing from within PS

--- Documents/WindowsPowerShell/Microsoft.Powershell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.Powershell_profile.ps1
@@ -12,12 +12,12 @@ function ssearch ([string]$text) {
         $global:__lastFile = $null
     }
 
-    echo $files
+    return $files
 }
 
 function s ([string]$text) {
     if ( $text -ne "" ) {
-        ssearch $text
+        return ssearch( $text )
     } elseif ( $global:__lastFile -ne $null ) {
         vim $global:__lastFile.path
     }
@@ -25,9 +25,58 @@ function s ([string]$text) {
 
 function sg ([string]$text) {
     if ( $text -ne "" ) {
-        s $text
+        return s( $text )
     } elseif ( $global:__lastFile -ne $null ) {
         gvim $global:__lastFile.path
     }
 }
 
+##
+## Author   : Roman Kuzmin
+## Synopsis : Resize console window/buffer using arrow keys
+## See      : http://stackoverflow.com/questions/153983/are-there-any-better-command-prompts-for-windows
+##
+
+function Size($w, $h) {
+    New-Object System.Management.Automation.Host.Size( $w, $h )
+}
+
+function resize() {
+    Write-Host '[Arrows] resize  [Esc] exit ...'
+    $ErrorActionPreference = 'SilentlyContinue'
+
+    for( $ui = $Host.UI.RawUI; ; ) {
+        $b = $ui.BufferSize
+        $w = $ui.WindowSize
+
+        switch ( $ui.ReadKey(6).VirtualKeyCode ) {
+            37 {
+                $w = Size ($w.width - 1) $w.height
+                $ui.WindowSize = $w
+                $ui.BufferSize = Size $w.width $b.height
+                break
+            }
+            39 {
+                $w = Size ($w.width + 1) $w.height
+                $ui.BufferSize = Size $w.width $b.height
+                $ui.WindowSize = $w
+                break
+            }
+            38 {
+                $ui.WindowSize = Size $w.width ($w.height - 1)
+                break
+            }
+            40 {
+                $w = Size $w.width ($w.height + 1)
+                if ($w.height -gt $b.height) {
+                    $ui.BufferSize = Size $b.width $w.height
+                }
+                $ui.WindowSize = $w
+                break
+            }
+            27 {
+                return
+            }
+        }
+    }
+}
